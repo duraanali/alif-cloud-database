0012f84aa6baae7dd7f00e44faa55bf919229826
Duraan Ali <duraan522@gmail.com>
2020-04-08 00:46:16 -0500

    heroku dep

--- data/seeds/04-classes.js
+++ data/seeds/04-classes.js
@@ -5,8 +5,7 @@ exports.seed = function (knex) {
     .then(function () {
       // Inserts seed entries
       return knex('classes').insert([
-        { id: 1, name: "Class 101", subject: "English", created: "09/27/2019", teacher_id: 1 },
-        { id: 2, name: "Class 102", subject: "Math", created: "09/27/2019", teacher_id: 1 }
+       
       ]);
     });
 };
