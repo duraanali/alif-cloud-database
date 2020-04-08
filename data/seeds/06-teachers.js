82678ca66153959e2029846292d5f06485f9e248
Duraan Ali <duraan522@gmail.com>
2020-04-08 00:23:04 -0500

    heroku dep

--- data/seeds/06-teachers.js
+++ data/seeds/06-teachers.js
@@ -5,7 +5,7 @@ exports.seed = function (knex) {
     .then(function () {
       // Inserts seed entries
       return knex('teachers').insert([
-       
+        { id: 1, name: "Duraan Ali", email: "duraan@gmail.com", password: "$2a$08$Q.NODULbt14MKc3mga7SheWHPe3Q3h5qDj8KpupSKwScp1rap5xDa" } // password: 123456789
       ]);
     });
 };
