a265356ca9339a7aa2d722f91e4d8b448b8b22d2
Duraan Ali <duraan522@gmail.com>
2020-04-08 22:49:15 -0500

    newapp

--- data/seeds/06-students.js
+++ data/seeds/06-students.js
@@ -1,7 +1,7 @@
 
 exports.seed = function (knex) {
   // Deletes ALL existing entries
-  return knex('students').truncate()
+  return knex('students').del()
     .then(function () {
       // Inserts seed entries
       return knex('students').insert([
